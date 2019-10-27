a9b0bcd287fb355084176388bb67d93195ead735
Ben <benlaugherty@gmail.com>
2019-10-27 15:18:03 -0700

    [add] Docstrings, formatting, cleaned up functions in histogram

--- src/histogram.py
+++ src/histogram.py
@@ -5,6 +5,19 @@ import re
 
 
 def get_clean_words(source_file):
+    """
+    Takes text file as a paramater and returns a list of all the words with all characters except for A-Z removed
+    Keeps spaces and newline characters
+
+    Params:
+        source_file: file - A .txt file to read words from
+
+    Returns:
+        List of words from a text file
+
+    Raises:
+        File Not Found if source file does not exist
+    """
     with open(source_file, 'r') as f:
         words_file = f.read()
         clean_words = re.sub(r'[^a-zA-Z\s]', '', words_file)
@@ -15,16 +28,15 @@ def get_clean_words(source_file):
 def histogram(source_file):
     """
     Takes text file as a paramater and returns a histogram data structure that stores each unique word along with
-    the number of times that the word appears in the source text.  Only accepts clean data
+    the number of times that the word appears in the source text
 
     Params:
-        source_file: .txt file that contains a corpus 
+        source_file: .txt file that contains text you want to get a histrogram of
 
     Returns:
-        Dict that contains unique words along with the number of times that word appears in text
+        words: dict - key of unique word and value of amount of times that word appears
     """
-    # Dictonary Implementation - Nothing can beat the performance of this.
-    # Also, this will not clean words - so please pass it a corpus that isn't messed up :)
+    # Dictonary Implementation - Esentially fastest implementation
     words = get_clean_words(source_file)
     histo = {}
     for word in words:
@@ -34,20 +46,36 @@ def histogram(source_file):
 
 
 @time_it
-def sort_histogram(dict_histogram):
+def sort_histogram(histogram):
     """
-    Takes a dictonary histogram and returns into a sorted list based off of amount of times a word appears
+    Sort a list or dictonary histogram with from greatest amount of appearances to least
+
+    Params:
+        histogram: dict, list - A list or dictonary histogram that you want to be sorted
+
+    Returns:
+        list: Sorted histogram as a list, or None if passed in value is not list or dictonary
     """
-    listed_histo = []
-    for key in dict_histogram.keys():
-        listed_histo.append([key, dict_histogram.get(key)])
-    return sorted(listed_histo, key=itemgetter(1), reverse=True)
+    if isinstance(histogram, dict):
+        listed_histo = []
+        for key in dict_histogram.keys():
+            listed_histo.append([key, dict_histogram.get(key)])
+        return sorted(listed_histo, key=itemgetter(1), reverse=True)
+    elif isinstance(histogram, list):
+        return sorted(histogram, key=itemgetter(1), reverse=True)
+    else:
+        return None
 
 
 @time_it
 def log_histrogram(histogram, filename='log.txt'):
     """
-    Log all entries in a histogram to a text file.
+    Log all entries in a histogram to a .txt file. Supports both dictonary and list histogram
+
+    Params:
+        histogram: dict, list - A list or dictionary histogram that you want to log to a file
+        filename: str - name of file that you want to log to
+            default: log.txt
     """
     # Try to remove the log file if there is a previous log file, otherwise just continue if the file doesn't exist
     try:
@@ -66,6 +94,15 @@ def log_histrogram(histogram, filename='log.txt'):
 
 @time_it
 def get_histogram_from_log(filename):
+    """
+    Get entries from a histogram log file and put them into a dictonary
+
+    Params:
+        filename: file - name of file that you want to read from
+
+    Returns:
+        dict: New histogram as a dictonary
+    """
     with open(filename, 'r') as f:
         # Remove any whitespace at beginning and end of file
         words = f.read().strip().split('\n')
@@ -76,9 +113,17 @@ def get_histogram_from_log(filename):
         return histo
 
 
-# This is a disgusting approach lmaoooo. Only about 950ms slower than my dictonary version :)
 @time_it
 def list_histogram(source_file):
+    """
+    Get a list version of a histogram from a text file
+
+    Params:
+        source_file: file - The file that you want to get a histogram of
+
+    Returns:
+        list: A list of lists that contain the amount of times that a word appears
+    """
     words = get_clean_words(source_file)
     histo = []
     for word in words:
@@ -92,28 +137,37 @@ def list_histogram(source_file):
     return histo
 
 
-# LMAOOOOOOOOOOOOOOOOOOOOOOOOOO
 @time_it
 def tuple_histogram(source_file):
-    words = get_clean_words(source_file)
-    histo = []
-    for word in words:
-        histo_entry = [word, 0]
-        for word2 in words:
-            if word in word2:
-                histo_entry[1] += 1
-        if histo_entry not in histo:
-            histo.append(histo_entry)
+    """
+    Get a tuple version of a histogram from a text file
 
+    Params:
+        source_file: file - The file you want to get a histogram of
+
+    Returns:
+        list: A list of tuples that contain the amount of a times the word appears
+    """
+    # TODO: Rewrite this to not use list_histogram
+    histo = list_histogram(source_file)
     tup = []
     for item in histo:
         tup.append(tuple(item))
 
     return tup
 
-# I am going to optimize these I swear
+
 @time_it
 def count_histogram(source_file):
+    """
+    Get a list of lists that contain a number and the words that are thast length
+
+    Params:
+        source_file: file - The file that you want to get a count histogram from
+
+    Returns:
+        list: A list of lists that contain the amount of times that a word appears
+    """
     words = get_clean_words(source_file)
     max_len = len(max(words, key=len))
     histo = []
@@ -129,13 +183,26 @@ def count_histogram(source_file):
 
 
 def unique_words(histogram):
+    """
+    Return the amount of unique words in a histogram.  Works for dicts, lists and tuples
+
+    Params:
+        histogram: dict, list, tuple - The histogram you want to get the unique amount of words from
+
+    Returns:
+        int: The amount of unique words in a histogram
+    """
     return len(histogram)
 
 
 def frequency(word, histogram):
+    """
+    Returns the amount of times a specific word appears in a histogram
+
+    Params:
+        histogram: dict - The histogram you want to read from
+
+    Returns:
+        int: The amount of times that the word appears
+    """
     return histogram.get(word, 0)
-
-
-if __name__ == "__main__":
-    histo = tuple_histogram('test.txt')
-    log_histrogram(histo)

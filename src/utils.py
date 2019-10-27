b0ed16ae27724742f901842a91cb05aea2a13fcf
Ben <benlaugherty@gmail.com>
2019-10-27 15:22:29 -0700

    [add] docstring to time_it function

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,15 @@
 import time
 
 def time_it(func):
+    """
+    A wrapper function that is used to get the time that a function takes
+    Made with <3 by Ben Lafferty
+
+    Use:
+        @time_it
+        def func():
+            return 'hi'
+    """
     # Made wth love by Ben <3 - DS2.3
     def wrapper(*args, **kwargs):
         start = time.time()

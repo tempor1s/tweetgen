bb2fe44ecde29b7a4f974d6f35f1f3ee9f635233
Ben <benlaugherty@gmail.com>
2019-10-27 15:30:34 -0700

    [add] docstrings, formatting, cleaned up functions in rearrange.py

--- src/rearrange.py
+++ src/rearrange.py
@@ -2,35 +2,42 @@ from sys import argv
 from random import shuffle, randint
 from utils import time_it
 
+
 @time_it
 def rearrange(arr):
-    # Fisher Yates Implementation
+    """
+    Rearrange a list.  Very similar to the built in python shuffle - Fisher yates
+
+    Params:
+        arr: list - The list you want to shuffle
+
+    Returns:
+        list: The shuffled list
+    """
     # Create a copy so that we are not modifiying the passed in array - slower but safe
     new_arr = list(arr)
     # Loop through the length of the array
     for i in range(len(new_arr)):
         # Chose a random value between 0 and int
         j = randint(0, i)
-        
+
         # Swap new_arr[i] with the element at random index using fancy python things reassignment :)
         new_arr[i], new_arr[j] = new_arr[j], new_arr[i]
 
     return new_arr
 
 
-def fisher_yates_shuffle(the_list):
-    amnt_to_shuffle = len(the_list)
-    while amnt_to_shuffle > 1:
-        i = int(floor(random() * amnt_to_shuffle))
-        amnt_to_shuffle -= 1
-        the_list[i], the_list[amnt_to_shuffle] = the_list[amnt_to_shuffle], the_list[i]
-    return the_list
-
-
 def lazy_rearrange(arr):
-    new_arr = list(arr)
-    shuffle(new_arr)
-    return ' '.join(new_arr)
+    """
+    Rearrage a list
+
+    Params:
+        arr: list - The list you want to shuffle
+
+    Returns:
+        list: The shuffled list 
+    """
+    return shuffle(list(new_arr(arr)))
 
 
 if __name__ == '__main__':

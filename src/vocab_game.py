bc27fc776061ab19bd348ff95c2d353ece309d9c
Ben <benlaugherty@gmail.com>
2019-10-27 15:21:00 -0700

    [add] doctstrings and formatting to the vocab game

--- src/vocab_game.py
+++ src/vocab_game.py
@@ -8,7 +8,16 @@ DICTONARY_API_KEY = os.getenv('DICTONARY_API_KEY')
 
 
 def get_random_word_and_def():
-    # Returns the defintion
+    """
+    Get a random word and definition
+
+    Returns:
+        word: str - The word that the defintion is for
+        definition: str - The defintion of the word
+
+        Format:
+            word, str
+    """
     while True:
         word = get_random_word()
         url = f'https://www.dictionaryapi.com/api/v3/references/collegiate/json/{word}?key={DICTONARY_API_KEY}'
@@ -21,7 +30,9 @@ def get_random_word_and_def():
 
 
 def main():
-    # word, definition = get_random_word_and_def(word)
+    """
+    Just a function that contains the logic to play the vocab game
+    """
     print('Welcome to my guessing game! Give me a second to fetch a defintion for you!')
     word, definition = get_random_word_and_def()
 
@@ -30,12 +41,11 @@ def main():
     print('\n')
 
     guess = input('Guess:  ')
-    
+
     if guess == word:
         print('Congrats! You won!')
     else:
         print(f'You lost... the word was {word}')
-
 
 
 if __name__ == '__main__':

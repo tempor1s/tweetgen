f3dd7de4f5891cb4c7782768f7f70e8add97396c
Ben <benlaugherty@gmail.com>
2019-10-30 21:38:54 -0700

    [add] specify sentence length with input field

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from lib.sample import get_sentence
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    sentence = get_sentence('lib/txt_files/sherlock.txt', amount=20)
-    return render_template('index.html', sentence=sentence)
+    num = int(request.args.get('num')) or 10
+    sentence = get_sentence('lib/txt_files/sherlock.txt', num)
+    return render_template('index.html', sentence=sentence, num=num)
 
 if __name__ == "__main__":
     app.run(debug=True)

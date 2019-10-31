920e615a564ee45793e47945fdcaf933bfbda30d
Ben <benlaugherty@gmail.com>
2019-10-30 20:53:50 -0700

    [add] basic templates, styles file, and boostrap

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 from lib.sample import get_sentence
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return get_sentence('lib/txt_files/test.txt')
+    sentence = get_sentence('lib/txt_files/test.txt')
+    return render_template('index.html', sentence=sentence)
 
 if __name__ == "__main__":
     app.run(debug=True)

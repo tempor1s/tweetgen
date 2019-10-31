d3ff344ec0b7225b38f648a17ec395cc47b7f4f2
Ben <benlaugherty@gmail.com>
2019-10-31 08:58:09 -0700

    [add] new buttons and route to be used with favorites

--- app.py
+++ app.py
@@ -3,17 +3,26 @@ from lib.sample import get_sentence
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         num = request.form.get('num')
         sentence = get_sentence('lib/txt_files/sherlock.txt', int(num))
-        
-        return jsonify({'sentence': sentence})
 
+        return jsonify({'sentence': sentence})
 
     sentence = get_sentence('lib/txt_files/sherlock.txt', 10)
     return render_template('index.html', sentence=sentence, num=10)
+
+
+@app.route('/favorites', methods=['GET', 'POST'])
+def favorites():
+    if request.method == 'POST':
+        pass
+
+    return render_template('favorites.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)

1e464ab2280322ff96c80339e7cb1e29e50956c6
Ben <benlaugherty@gmail.com>
2019-11-02 11:58:35 -0700

    [add] cleaned up javascript

--- static/script.js
+++ static/script.js
@@ -1,34 +1,30 @@
 $(document).ready(function() {
-    $("#generate").on('click', function() {
+    $('#generate').on('click', function() {
         let val = $('#num_field');
         let num = val[0].value;
 
-        let checked = $('#vowel_field').is(':checked');
         let vowel = 'False';
+        let checked = $('#vowel_field').is(':checked');
         // So that it isnt 'true' and instead is True
         if (checked) {
             vowel = 'True';
         }
-        console.log(vowel);
         req = $.ajax({
             url: '/',
             type: 'POST',
-            data: {num: num, vowel: vowel}
-        })
+            data: { num: num, vowel: vowel }
+        });
         req.done(function(data) {
             $('#sentence').text(data.sentence);
         });
     });
 
-    $("#favorite").on('click', function() {
+    $('#favorite').on('click', function() {
         let sentence = $('#sentence').text();
         req = $.ajax({
             url: '/favorites',
             type: 'POST',
-            data: {sentence: sentence}
-        })
-        req.done(function(data) {
-            console.log('req done')
+            data: { sentence: sentence }
         });
     });
 });

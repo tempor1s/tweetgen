7a5292a73f5211163bd6de0f012ceb53a44541ee
Ben <benlaugherty@gmail.com>
2019-11-08 14:36:29 -0700

    [add] working creation of a markov chain

--- lib/markov.py
+++ lib/markov.py
@@ -6,18 +6,29 @@ class MarkovChain(dict):
     def __init__(self, word_list):
         super().__init__() # Initialize this as a new dict
 
-        # if word_list:
-        #     for i, word in enumerate():
-        #         word = self.get(word, None)
-        #         if word:
-        #             word.add_word(word, 1)
-        #         else:
-        #             self[word] = Dictogram(list(word_list[i + 1]), False)
-                    
+        # Check to make sure a word list is passed in
+        if word_list:
+            for i in range(0, len(word_list)):
+                # The current word in the iteration
+                word = word_list[i]
+                
+                try:
+                    # The word 1 index ahead of the current word, aka the next word in the sentence
+                    # has to be in try except because of index out of range exception on end of list
+                    next_word = word_list[i + 1]
+                # If index error then no new words in list to add so break out    
+                except IndexError:
+                    break
 
-    def add_histo(self, word):
-        "Add a word / histo combo. Should work for if word already exists or of it does not."
-        pass
+                # The word dictogram if it exists
+                word_dicto = self.get(word, None)
+
+                if word_dicto is not None:
+                    # Add a new word entry to the dictogram if the dictogram already exists
+                    word_dicto.add_count(next_word, 1)
+                else:
+                    # Create a new dictogram for the word
+                    self[word] = Dictogram([next_word])
 
     def sample(self, word):
         """
@@ -31,5 +42,6 @@ class MarkovChain(dict):
 
 if __name__ == "__main__":
     words = get_clean_words('txt_files/example.txt')
+    print(words)
     chain = MarkovChain(words)
     print(chain)
